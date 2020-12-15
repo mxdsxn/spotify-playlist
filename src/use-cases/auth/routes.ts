c181f256db4056951544d88b286642b2267ad3f4
Madson Santana <madson.santana@dextra-sw.com>
2020-12-14 22:46:57 -0300

    feat(auth): create routes to auth

---
+++ src/use-cases/auth/routes.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+
+const authRouter = express.Router()
+
+authRouter.post('/register', async (req, res,) => {
+
+},)
+
+authRouter.post('/login', async (req, res,) => {
+
+},)
+
+authRouter.post('/forgot-password', async (req, res,) => {
+
+},)
+
+authRouter.post('/reset-password', async (req, res,) => {
+
+},)
