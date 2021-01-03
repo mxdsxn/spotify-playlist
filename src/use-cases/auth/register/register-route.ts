d90ae0fe9aa4c578c3e9158b9f9874f9701df5f4
Madson Santana <madson.santana@dextra-sw.com>
2021-01-03 19:50:43 -0300

    chore(eslint): update lint

--- src/use-cases/auth/register/register-route.ts
+++ src/use-cases/auth/register/register-route.ts
@@ -1,23 +1,46 @@
-import express from 'express'
+import express, {
+  Request, Response, 
+} from 'express'
+import {
+  checkSchema, 
+} from 'express-validator'
 import registerUser from './register-service'
+
+const validatorRoute = checkSchema({
+  name: {
+    in: [ 'body', ],
+    isString: true,
+    notEmpty: true,
+  },
+  email: {
+    in: [ 'body', ],
+    isEmail: true,
+    notEmpty: true,
+  },
+  password: {
+    in: [ 'body', ],
+    isString: true,
+    notEmpty: true,
+  },
+},)
 
 const registerRoute = express.Router()
 
-registerRoute.post('/register', async (req, res,) => {
+registerRoute.post('/register', validatorRoute, async (req: Request, res: Response,) => {
   try {
     const result = await registerUser(req.body,)
 
     const statusCode = result.resources !== null ? 200 : 409
     return res
-      .status(statusCode,)
-      .json(result,)
+        .status(statusCode,)
+        .json(result,)
   } catch (error) {
     return res
-      .status(400,)
-      .json({
-        message: 'Erro em /register.',
-        error,
-      },)
+        .status(400,)
+        .json({
+          message: 'Erro em /register.',
+          error,
+        },)
   }
 },)
 
