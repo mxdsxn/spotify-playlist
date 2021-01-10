ed4b16d0f2485251f3f6c71b3ce18db0c2ff5c29
Madson Santana <madson.santana@dextra-sw.com>
2021-01-10 18:12:03 -0300

    dependencies(tests): set jest

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -1,12 +1,12 @@
 module.exports = {
- mongodbMemoryServerOptions: {
-  instance: {
-   dbName: "jest",
-  },
-  binary: {
-   version: '4.0.3',
-   skipMD5: true,
-  },
-  autoStart: false,
- }
+  mongodbMemoryServerOptions: {
+    instance: {
+      dbName: "spotify-playlist-test-db",
+    },
+    binary: {
+      version: '4.0.3',
+      skipMD5: true,
+    },
+    autoStart: false,
+  }
 }
