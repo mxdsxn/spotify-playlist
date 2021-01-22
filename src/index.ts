008db7e94ee52ee6ac2fbaa8ff55d33bfa7a3230
Madson Santana <madson.santana@dextra-sw.com>
2021-01-22 11:38:40 -0300

    feat(env): export env variables

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import 'dotenv/config'
-
+// import 'dotenv/config'
 import server from './server'
+import envs from './config/env'
 
-const { SERVER_PORT } = process.env
+const { SERVER_PORT } = envs
 
 server.listen(SERVER_PORT, () => {
-  console.log(`Server listening on port ${SERVER_PORT}`)
+  global.console.log(`Server listening on port ${SERVER_PORT}`)
 })
