063e91328fe18708754bdc72212fec57a59bfb71
madson.santana <madson.santana96@gmail.com>
2021-02-07 00:33:27 -0300

    feat(playlist): insert track in playlist adjustment

--- src/use-cases/playlist/insert-track-playlist/service.ts
+++ src/use-cases/playlist/insert-track-playlist/service.ts
@@ -1,16 +1,20 @@
-import { resultInterface } from '@interfaces'
+import {
+  resultInterface, trackInterface,
+} from '@interfaces'
 import {
   PlaylistSchema, TrackSchema,
 } from '@schemas'
+import axios from 'axios'
 
 interface optionsInterface {
+  spotifyToken: string,
   trackId: string
   playlistId: string,
 }
 
 const updatePlaylist = async (options: optionsInterface): Promise<resultInterface> => {
   try {
-    const playlist = await PlaylistSchema.findById(options.playlistId)
+    const playlist = await PlaylistSchema.findById(options.playlistId).populate('tracks')
 
     if (!playlist) {
       const result: resultInterface = {
@@ -20,16 +24,33 @@ const updatePlaylist = async (options: optionsInterface): Promise<resultInterfac
       return result
     }
 
+    const oldTracks = playlist.get('tracks') as Array<trackInterface>
+    if (oldTracks.some(track => track.spotifyId === options.trackId)) {
+      const result: resultInterface = {
+        hasError: true,
+        message: 'Track já pertence à playlist.',
+      }
+      return result
+    }
+
     let track = await TrackSchema.findOne({ spotifyId: options.trackId })
     if (!track) {
-      track = await TrackSchema.create({
-        name: 'trackname', spotifyId: options.trackId, artist: 'artist',
+      const trackSpotify = await getTrack({
+        trackId: options.trackId,
+        spotifyToken: options.spotifyToken,
       })
 
-    }
-    global.console.log(track, 'new')
+      const { resources } = <{ resources: trackSpotifyInterface }>trackSpotify
 
-    // await playlist.save()
+      track = await TrackSchema.create({
+        name: resources.name, spotifyId: resources.id, artist: resources.artists[0].name,
+      })
+    }
+
+    const newTracks = oldTracks.push(track)
+    playlist.set({ newTracks })
+
+    await playlist.save()
 
     const result: resultInterface = {
       hasError: false,
@@ -39,9 +60,44 @@ const updatePlaylist = async (options: optionsInterface): Promise<resultInterfac
   } catch (error) {
     const result: resultInterface = {
       hasError: true,
-      message: 'Falha ao alterar a playlist.',
+      message: 'Falha ao adicionar track a playlist.',
     }
     return result
   }
 }
 export default updatePlaylist
+
+interface trackSpotifyInterface {
+  name: string,
+  id: string,
+  artists: [{
+    id: string,
+    name: string,
+  }]
+}
+
+interface trackOptionsInterface {
+  spotifyToken: string,
+  trackId: string
+}
+
+const getTrack = async (options: trackOptionsInterface): Promise<resultInterface> => {
+  const baseUrl = `https://api.spotify.com/v1/tracks/${options.trackId}`
+
+  try {
+    const track = await axios.get(baseUrl, { headers: { 'Authorization': `Bearer ${options.spotifyToken}` } })
+
+    const result: resultInterface = {
+      hasError: false,
+      resources: track.data,
+    }
+
+    return result
+  } catch (error) {
+    const result: resultInterface = {
+      message: 'Falha ao buscar track.',
+      hasError: true,
+    }
+    return result
+  }
+}
