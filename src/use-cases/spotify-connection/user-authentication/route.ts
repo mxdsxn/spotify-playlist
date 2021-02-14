b45f48b597472968054367efa7aeb928ad6b186a
madson.santana <madson.santana96@gmail.com>
2021-02-14 13:56:41 -0300

    test(user-authentication): set test

--- src/use-cases/spotify-connection/user-authentication/route.ts
+++ src/use-cases/spotify-connection/user-authentication/route.ts
@@ -5,8 +5,7 @@ import { checkSchema } from 'express-validator'
 import {
   validatorMiddleware, responseHandler,
 } from '@common'
-import { UserSchema } from '@schemas'
-import spotifyService from './service'
+import setUserAuthenticationToken from './service'
 
 const validationRoute = checkSchema({
   code: {
@@ -20,12 +19,9 @@ const userAuthenticationRoute = Router()
 userAuthenticationRoute.get('/authentication-token', validationRoute, validatorMiddleware, async (req: Request, res: Response, next: NextFunction) => {
   try {
     const codeAuthorization = req.query.code as string
-
-    const result = await spotifyService.getAppAuthenticationUrl(codeAuthorization)
-
-    const { access_token } = result.resources
     const { userId } = req.body
-    await UserSchema.findByIdAndUpdate(userId, { spotifyToken: access_token })
+
+    const result = await setUserAuthenticationToken(userId, codeAuthorization)
 
     return await responseHandler(res, result)
   } catch (error) {
--- src/use-cases/spotify-connection/user-authentication/service.ts
+++ src/use-cases/spotify-connection/user-authentication/service.ts
@@ -2,6 +2,7 @@ import axios from 'axios'
 import { envs } from '@config'
 import { resultInterface } from '@interfaces'
 import { errorHandler } from '@common'
+import { UserSchema } from '@schemas'
 
 const {
   SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, SPOTILIST_API_REDIRECT_URI,
@@ -9,7 +10,10 @@ const {
 
 const SPOTIFY_TOKEN_URL = 'https://accounts.spotify.com/api/token'
 
-const getAppAuthenticationUrl = async (codeAuthorization: string): Promise<resultInterface> => {
+const setUserAuthenticationToken = async (userId: string, codeAuthorization: string): Promise<resultInterface> => {
+  console.log({
+    userId, codeAuthorization,
+  })
   const bodyParams = {
     client_id: SPOTIFY_CLIENT_ID,
     client_secret: SPOTIFY_CLIENT_SECRET,
@@ -20,15 +24,18 @@ const getAppAuthenticationUrl = async (codeAuthorization: string): Promise<resul
 
   try {
     const getTokenResult = await axios.post(SPOTIFY_TOKEN_URL, null, { params: bodyParams })
-    const result: resultInterface = {
-      resources: getTokenResult.data,
-      statusCode: getTokenResult.status,
-    }
 
+    const { data: { access_token } } = getTokenResult
+    console.log({ access_token })
+    await UserSchema.findByIdAndUpdate(userId, { spotifyToken: access_token })
+
+    const result: resultInterface = { statusCode: 200 }
     return result
   } catch (error) {
+    console.log({ error })
+
     return await errorHandler(error, 'spotify token error.')
   }
 }
 
-export default { getAppAuthenticationUrl }
+export default setUserAuthenticationToken
