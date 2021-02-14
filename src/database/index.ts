c0888af77ed69f97ae538394d577cd4760431deb
madson.santana <madson.santana96@gmail.com>
2021-02-14 16:43:39 -0300

    test(jest.config): set shelf preset in jest.config to test mongodb without mock

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,8 +1,17 @@
 import mongoose from 'mongoose'
+import { envs } from '@config'
+
+const {
+  DB_NAME, DB_HOST, NODE_ENV, MONGO_URL,
+} = envs
+
+const mongoUrl = NODE_ENV === 'test' && !!MONGO_URL
+  ? MONGO_URL
+  : `mongodb://${DB_HOST}/${DB_NAME}`
 
 const mongoConnection = mongoose
 
-mongoConnection.connect('mongodb://localhost/spotilist', {
+mongoConnection.connect(mongoUrl, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
   useCreateIndex: true,
