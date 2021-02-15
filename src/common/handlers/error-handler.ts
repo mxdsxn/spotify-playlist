7d82a095e18f3ed3e83c15e3e1f0cb21aba9c971
madson.santana <madson.santana96@gmail.com>
2021-02-14 21:00:04 -0300

    test(insert-track-playlist): set service's test

--- src/common/handlers/error-handler.ts
+++ src/common/handlers/error-handler.ts
@@ -2,8 +2,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { resultInterface } from '@interfaces'
+import { envs } from '@config'
 
-const errorHandler = async (_error: any, message?: string): Promise<resultInterface> => {
+const { NODE_ENV } = envs
+
+const errorHandler = async (error: any, message?: string): Promise<resultInterface> => {
+  NODE_ENV === 'test' && global.console.log(error)
+
   const result: resultInterface = {
     hasError: true,
     message: message || 'service error.',
