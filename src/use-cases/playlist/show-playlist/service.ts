0817faefc7d52f23d23e82f67ad93cb658bc9abd
madson.santana <madson.santana96@gmail.com>
2021-02-16 20:41:01 -0300

    test(show-playlist): set service's test

--- src/use-cases/playlist/show-playlist/service.ts
+++ src/use-cases/playlist/show-playlist/service.ts
@@ -2,7 +2,7 @@ import { errorHandler } from '@common'
 import { resultInterface } from '@interfaces'
 import { PlaylistSchema } from '@schemas'
 
-const showPlaylist = async (playlistId: string): Promise<resultInterface> => {
+const showPlaylist = async (userId: string, playlistId: string): Promise<resultInterface> => {
   try {
     const playlist = await PlaylistSchema.findById(playlistId).populate('tracks')
 
@@ -15,10 +15,23 @@ const showPlaylist = async (playlistId: string): Promise<resultInterface> => {
       return result
     }
 
-    const result: resultInterface = {
-      resources: playlist,
-      statusCode: 200,
+    const isPrivate = playlist.get('isPrivate')
+
+    if ((String(playlist.userId) === String(userId)) || !isPrivate) {
+      const result: resultInterface = {
+        resources: playlist,
+        statusCode: 200,
+      }
+
+      return result
     }
+
+    const result: resultInterface = {
+      hasError: true,
+      statusCode: 401,
+      message: 'unauthorized.',
+    }
+
     return result
   } catch (error) {
     return await errorHandler(error, 'show playlist error.')
